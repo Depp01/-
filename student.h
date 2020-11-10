f22a52fda1ce2609435faedce58abd4d134c2224
Depp01 <58043943+Depp01@users.noreply.github.com>
2020-11-10 20:26:50 +0200

    Game changing

--- student.h
+++ student.h
@@ -6,10 +6,6 @@ class student{
     string depratment; 
 
 }
-
-string student_name[10] = {"Mhialdeen", "Mohammd", "Ali", "Seif", "Fatima", "Waad", "Ammar", "hessen", "Ahmed", "Eva"};
-int student_uni_id[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-int student_degree[10] = {99, 89, 79, 69, 59, 49, 39,29,19, 9};
-char student_depratment[10] = [C, C, A, C, C, C, C, C, T, T];
-
-string 
+int grade_average(int student _degree[i], int subject)
+{
+    return int student_degree[i] / int subject;
