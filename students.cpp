4b2773387b1094b988af16967e07b57466b061df
Depp01 <58043943+Depp01@users.noreply.github.com>
2020-11-10 20:20:47 +0200

    ""

--- students.cpp
+++ students.cpp
@@ -55,6 +55,6 @@ int student_uni_id[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 int student_degree[10] = {99, 89, 79, 69, 59, 49, 39,29,19, 9};
 char student_depratment[10] = [C, C, A, C, C, C, C, C, T, T];
 
-string subject[8] = {subject1, subject2, subject3, subject4, subject5, subject6, subject7, subject8, subject9, subject10}  
+string subject[8] = {"subject1", "subject2", "subject3", "subject4", "subject5", "subject6", "subject7", "subject8", "subject9", "subject10"}  
 }
 
