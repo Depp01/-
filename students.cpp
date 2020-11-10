a4838fc011dab01bb35ebefd7e16a1154166bc13
Depp01 <58043943+Depp01@users.noreply.github.com>
2020-11-10 20:18:13 +0200

    "F"

--- students.cpp
+++ students.cpp
@@ -50,7 +50,7 @@ int main()
     //cout<<"name: "<<student5.name\n<<"uni_id: "<<student5.uni_id\n<<"degree: "<<student5.degree\n<<"depratment: "<<student5.depratment<<endl;
 
 
-string student_name[10] = {Mhialdeen, Mohammd, Ali, Seif, Fatima, Waad, Ammar, hessen, Ahmed, Eva};
+string student_name[10] = {"Mhialdeen", "Mohammd", "Ali", "Seif", "Fatima", "Waad", "Ammar", "hessen", "Ahmed", "Eva"};
 int student_uni_id[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 int student_degree[10] = {99, 89, 79, 69, 59, 49, 39,29,19, 9};
 char student_depratment[10] = [C, C, A, C, C, C, C, C, T, T];
